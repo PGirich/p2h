985f0de65913eab85756c9087f84d3378c90d2d3
PGirich <petr.girich@gmail.com>
2022-04-22 20:49:33 +0300

    4 files modified

--- p2h.js
+++ p2h.js
@@ -1,6 +1,83 @@
-// Import stylesheets
-import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+ import {loadActionsList, actionsList, hero} from "./p2hdatamodel.js";
+
+ const $panelStat = document.getElementById("panelStat")
+
+ loadActionsList()
+ showActions()
+
+ function showActions() {
+     let htmlCode = `actions <br>`;
+     actionsList.forEach(action => {
+         const btId = "action.${action.name}";
+         const $btAction = $panelStat.getElementByID(btId);
+         if (btAction === null) {
+           btAction = $panelStat.createElement("button");
+           btAction.id = "action.${action.name}";
+           btAction.class = "actionButton";
+           btAction.data-tooltip = "${action.comment}";
+           btAction.onmouseover = ()->{showHint(e,'${action.name}')};
+           btAction.onmouseout = ()->{hideHint(e,'${action.name}')};
+           btAction.caption = action.caption;
+         }
+
+              <br>`
+     })
+     panelStat.innerHTML = htmlCode
+     console.log(`show actions `+htmlCode)
+ }
+
+ console.log(`hello world`)
+// document.write("<h1>hello world"+1);
+// document.write("<h1>grandpa! "+2);
+// document.write("<h2>mouse! "+(2*2+3-5));
+// document.write("<h3>dog! "+(2*2+3-4));
+// document.write("<h421:23 08.12.2019>bird! "+(2*2+3-3));
+// document.write("<br>grandma");
+
+// martial artist
+const artist = {
+    name: '',
+    isLive: true
+}
+
+// main character
+
+
+
+const action = {
+    name : `action name`,
+//    doAction() {console.log(`error: abstract do action`)},
+    enabled : false,
+//    checkEnabled() {enabled = false}
+}
+
+const fillBar = {
+    name: `ki name`
+
+
+}
+
+const fillBarAction = {
+    name: `feel ki`
+}
+
+
+/* hint mechanic */
+export function showHint (e,controlName) {
+    const control = Document.getElementByID('controlName')
+    const tooltip = Document.getElementByID('tooltip')
+    tooltip.top = e.pageY + 5
+    tooltip.left = e.pageX + 5
+    tooltip.innerText = control.data-tooltip
+    tooltip.show()
+    console.log(`show tooltip`)
+}
+ export function hideHint (e,controlName) {
+     const control = Document.getElementByID('controlName')
+     const tooltip = Document.getElementByID('tooltip')
+     tooltip.top = 0
+     tooltip.left = 0
+     tooltip.hide()
+     console.log(`hide tooltip`)
+ }
--- p2hdatamodel.js
+++ p2hdatamodel.js
@@ -1,5 +1,79 @@
-{
-  "compilerOptions": {
-    "target": "esnext"
+
+// main character
+export const hero = {
+  actions : []
+}
+
+//
+class Action {
+  constructor(actionType) {
+      this.type = actionType;
+      this.actionListEntry = actionsList.findIndex()
+      this.ena = true;
+      this.vis = true;
+      this.width = width;
   }
+}
+
+// Add action to hero
+export function addAction(actionName) {
+  const actionListEntry = actionsList.find(element => element.name === actionName)
+  hero.actions.push(actionListEntry)
+  hero[actionName].ena = actionListEntry.IsEna
+}
+
+// Apply cost to hero
+// cost format:  [{prop:'hero object property name', val:`cost to subtract`},...]
+export function applyResult(arrPropCost) {
+  arrPropCost.forEach(propCost => {hero[propCost.prop]-=propCost.val})
+}
+
+// Check condition fulfillment
+// condition format:  [{prop:'hero object property name',op=`< > =`, val:`value to check`},...]
+export function checkCondition(arrPropOpVal) {
+  let result = (arrPropOpVal===undefined)||(arrPropOpVal.length===0)
+  if (!result) {
+      arrPropOpVal.forEach(propOpVal => {
+          result &&= eval(propOpVal`hero.${propOpVal.prop}${propOpVal.op}${propOpVal.val}`)
+      })
+  }
+  return result
+}
+
+// ====================================================
+// full list of all actions
+export const actionsList = []
+export function loadActionsList() {
+  actionsList.push({
+      name: "begging",
+      caption: "begging",
+      comment: "begging passersby for money",
+      addCond: [],
+      enableCond: [],
+      ticResult: [
+          {prop:'stamina',cost:0.3}
+      ],
+      actionTime: 8,
+      actionResult: [
+          {prop:'gold',cost:0.01}
+      ]
+  })
+  actionsList.push({
+      name: "learning_reading",
+      caption: "learning reading",
+      comment: "learning how to read with old teacher",
+      addCond: [],
+      enableCond: [
+          {prop:'filth',op:`<`, val:1}
+      ],
+      ticResult: [
+          {prop:'stamina',cost:0.3}
+      ],
+      actionTime: 200,
+      actionResult: [
+          {prop:'scholar',cost:0.01},
+          {prop:'scholar',cost:0.01}
+      ]
+  })
+  console.log(actionsList)
 }
